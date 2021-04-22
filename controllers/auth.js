4cead04bac125347d50b9958370e373a2bcc349d
yasser tamimi <altamimiy89@gmail.com>
2021-04-22 06:34:24 +0300

    fixed bugs in register validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,29 @@
-exports.register = (req, res) => {
-  res.json({ body: req.body });
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create user
+    const newUser = new User({ email, password: hashedPassword });
+
+    // save user
+    const savedUser = await newUser.save();
+
+    // create token
+    const token = jwt.sign({ user: savedUser._id }, process.env.TOKEN_SECRET);
+
+    // return token in cookie
+    res.cookie('token', token, { httpOnly: true }).send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: [{ msg: 'Server error' }] });
+  }
 };
--- utils/validation.js
+++ utils/validation.js
@@ -1,14 +1,25 @@
 const { check, validationResult } = require('express-validator');
 
+const User = require('../models/User');
+
 exports.validateRegister = [
-  check('email').isEmail().withMessage('Invalid Email'),
-  check('password')
-    .isLength({ min: 6 })
-    .withMessage('password must be at least 6 characters long'),
+  check('email', 'Invalid Email')
+    .isEmail()
+
+    // custom to check if email exists in database
+    .custom(async (value) => {
+      const existingEmail = await User.findOne({ email: value });
+      if (existingEmail) return Promise.reject('Email already exists');
+    }),
+  check('password', 'password must be at least 6 characters long').isLength({
+    min: 6,
+  }),
   (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty())
+    if (!errors.isEmpty()) {
+      console.log(req.body.email);
       return res.status(400).json({ errors: errors.array() });
+    }
 
     next();
   },
