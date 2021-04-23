b0b5f64809f73d4b813a792d72c744ac962694ae
yasser tamimi <altamimiy89@gmail.com>
2021-04-23 06:25:40 +0300

    fixed import path bug

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 require('dotenv').config();
 
